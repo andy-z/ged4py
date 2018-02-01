9bdf63f86ea00a7fe349ef78a10e6707bef334a4
Andy Salnikov <andy@salnikov.us>
2018-01-31 23:10:22 -0800

    Bump version: 0.1.3 → 0.1.4

--- ged4py/__init__.py
+++ ged4py/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Andy Salnikov"""
 __email__ = 'ged4py@py-dev.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from . import codecs  # noqa: F401, needed to register ANSEL codec
 from .parser import GedcomReader  # noqa: F401
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='ged4py',
-    version='0.1.3',
+    version='0.1.4',
     description="GEDCOM tools for Python",
     long_description=readme + '\n\n' + history,
 
