545cf79ee8f28ad20d437e76c5698f0879da9090
Andy Salnikov <andy@salnikov.us>
2020-08-16 01:12:44 -0700

    Bump version: 0.2.1 → 0.2.2

--- ged4py/__init__.py
+++ ged4py/__init__.py
@@ -27,4 +27,4 @@ _ansel.register()
 
 __author__ = """Andy Salnikov"""
 __email__ = 'ged4py@py-dev.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='ged4py',
-    version='0.2.1',
+    version='0.2.2',
     description="GEDCOM tools for Python",
     long_description=readme + '\n\n' + history,
 
