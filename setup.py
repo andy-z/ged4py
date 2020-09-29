4f7d64b5961b298b20bc538c9140d13f316bcbf2
Andy Salnikov <andy@salnikov.us>
2020-09-28 21:21:04 -0700

    Bump version: 0.2.4 → 0.3.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='ged4py',
-    version='0.2.4',
+    version='0.3.0',
     description="GEDCOM tools for Python",
     long_description=readme + '\n\n' + history,
 
